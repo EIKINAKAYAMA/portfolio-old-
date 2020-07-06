8e4f6d77fdff641144a835cbac85dd5e8e7d324e
EIKINAKAYAMA <EIKINAKAYAMA@users.noreply.github.com>
2020-07-06 23:10:14 +0900

    admin_function1(about_page)
    created_about_page(front and admin page)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image]) 
   # end
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
