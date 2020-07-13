1f75866f736130f244c41a510a85f6ac69d4bdd8
EIKINAKAYAMA <EIKINAKAYAMA@users.noreply.github.com>
2020-07-13 21:38:12 +0900

    admin_function2
    admin_function2(customerspage_structure)

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,20 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root "users#index"
+
   get 'users/index', to: 'users#index'
   get 'users/:id/show_admin', to: 'users#show_admin'
   get 'users/:id/show_front', to: 'users#show_front'
-  resources :users, only: [:edit, :update]
+
+  # index page doesn't require "index" just "customers"
+  # so make it easier
+
+  # get 'customers/:id/new', to: 'customers#new'
+  # get 'customers/create/:id', to: 'customers#create'
+  # post 'customers/create/:id', to: 'customers#create'
+  # get 'customers/index', to: 'customers#index'
+
+  resources :users, only: [:edit, :update] do
+    resources :customers, only: [:new, :create, :show]
+  end
 end
