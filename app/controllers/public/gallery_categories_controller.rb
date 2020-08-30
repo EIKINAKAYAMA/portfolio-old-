edd0e031d02d6fe625d613b7e635af38350c2474
EIKINAKAYAMA <EIKINAKAYAMA@users.noreply.github.com>
2020-08-30 14:07:47 +0900

    create_categories(public)

--- app/controllers/public/gallery_categories_controller.rb
+++ app/controllers/public/gallery_categories_controller.rb
@@ -1,2 +1,6 @@
 class Public::GalleryCategoriesController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @gallery_categories =GalleryCategory.where(user_id: params[:user_id])
+  end
 end
