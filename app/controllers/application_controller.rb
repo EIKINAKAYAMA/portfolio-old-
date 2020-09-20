50f877b3bc712fb09991e6069b9f7eb998ec6b2c
EIKINAKAYAMA <EIKINAKAYAMA@users.noreply.github.com>
2020-09-21 00:24:55 +0900

    basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
-  
   # # if you want to require image when users sign up, use below codes
   # def configure_permitted_parameters
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image]) 
@@ -12,18 +10,5 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, profile_attributes: [:user_image, :user_comment]])
-  end
-
-  private
-
-  # 以下、basic認証用のコード
-  def production?
-    Rails.env.production?
-  end
-
-  def basic_auth
-    authenticate_or_request_with_https_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
   end
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -61,7 +61,3 @@
 #   }
 
 server '54.178.65.106', user: 'ec2-user', roles: %w{app db web}
-
-# basic認証用
-set :rails_env, "production"
-set :unicorn_rack_env, "production"
