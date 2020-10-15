6526d29dc9f56691868a652d51e14dcbc8845408
EIKINAKAYAMA <EIKINAKAYAMA@users.noreply.github.com>
2020-10-15 20:19:28 +0900

    TEST
    TEST

--- app/assets/javascripts/admin/instagram.js
+++ app/assets/javascripts/admin/instagram.js
@@ -2,7 +2,7 @@ $(function () {
   const urlParams = new URLSearchParams(window.location.search);
   const code = urlParams.get('code');
   const url = "https://api.instagram.com/oauth/access_token"
-  const instagram_array = []
+  const instagram_array = [[]]
   
   //popup.jsで定義した関数の読み込み
   var buildpopupInstagram = window.hogeLib.buildpopupInstagram();
@@ -64,7 +64,7 @@ $(function () {
             .then(response => response.blob())
             .then(blob => new File([blob], "Instagram" + url + ".mov", { type: "video/mov" }))
             .then(file => {
-                instagram_array[index].push([])
+                instagram_array.push([])
                 instagram_array[index][0] = file
               })
 
@@ -77,15 +77,14 @@ $(function () {
               .then(response => response.blob())
               .then(blob => new File([blob], "Instagram" + url + ".jpeg", { type: "image/jpeg" }))
               .then(file => {
-                instagram_array[index].push([])
+                instagram_array.push([])
                 instagram_array[index][0] = file
               })
             //media_typeがCAROUSEL_ALUBUMの場合
           } else if (instagram.media_type == "CAROUSEL_ALBUM") {
-
+            instagram_array.push([])
             instagram.children.data.forEach(function (children, index2) {
               var url = new URLSearchParams(children.media_url).get('oe');
-              instagram_array[index].push([])
               if (children.media_type == "IMAGE") {
                 $('.image_list').append(buildImg(children.media_url, index, index2));
                 fetch(children.media_url)
